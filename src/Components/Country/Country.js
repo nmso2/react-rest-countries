ec81d5d5f1f0732be724b143cb20eb348f2ff1bc
Md. Nagib Mahfuz Subho <nmsubho@gmail.com>
2021-09-20 18:51:27 +0600

    rest countries api

---
+++ src/Components/Country/Country.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Country.css'
+
+const Country = (props) => {
+    return (
+        <div className="country">
+            <h2>Name: {props.country.name}</h2>
+            <img width="200px" src={props.country.flag} alt="" />
+            <p>Region: {props.country.region}</p>
+            <p>Population: {props.country.population}</p>
+        </div>
+    );
+};
+
+export default Country;
